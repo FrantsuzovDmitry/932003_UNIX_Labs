633dda63245f3b0a184d467b3aeb07ac5ab5a4d1
FrantsuzovDmitry <81227412+FrantsuzovDmitry@users.noreply.github.com>
2023-10-04 12:40:47 +0700

    Update and rename Lab1.sh to Lab1_var1.sh

---
+++ Lab1/Lab1_var1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+[[ ! "$1" =~ \.c$|\.cpp$ ]] && echo "File cannot be compiled" >&2 && exit 2
+
+temp_dir=$(mktemp -d) || exit 1
+trap 'rm -rf "$temp_dir"' EXIT HUP INT QUIT TERM PIPE
+LANG=C
+out_file=$((grep '// Output:' $1 || grep '//Output:' $1 || grep '// output:' $1 || 
+grep '//output:' $1) | cut -d ':' -f 2)
+[ -z "$out_file" ] && echo "File name not found in the file" >&2 && exit 3;
+
+gcc -o "$temp_dir/$out_file" "$1" -lstdc++ && mv "$temp_dir/$out_file" ./
+echo "Successful"
