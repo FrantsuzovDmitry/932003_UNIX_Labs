5179799d01418f4ec58feace741609c6ee2a0155
Dmitry <francuzov_dm@mail.ru>
2023-10-25 22:38:07 +0700

    Add 3 lab

---
+++ Lab3/main.cpp
@@ -0,0 +1,49 @@
+#include <QApplication>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QWidget>
+
+int main(int argc, char* argv[]) {
+    
+    QApplication app(argc, argv);                       // Initialization
+
+    QWidget window;                                     // Some kind of Form
+    window.resize(400, 200);
+
+    QVBoxLayout* layout = new QVBoxLayout(&window);     // Vertical layout on window
+
+    QLineEdit* lineEdit = new QLineEdit(&window);       // Edit field
+    lineEdit->setFixedHeight(35);
+    layout->addWidget(lineEdit);
+
+    QPushButton* sadButton = new QPushButton("Sad button :(", &window);
+    sadButton->setFixedHeight(50);
+    layout->addWidget(sadButton);
+
+    QPushButton* exitButton = new QPushButton("Exit", &window);
+    exitButton->setFixedHeight(50);
+    layout->addWidget(exitButton);
+
+    // Exit to click on "Exit"
+    QObject::connect(exitButton, SIGNAL(clicked()), &app, SLOT(quit()));
+
+    // Behaviour to click on "Sad button"
+    bool isSad = true;
+    QObject::connect(sadButton, &QPushButton::clicked, [&isSad, sadButton, lineEdit]() {
+        if (isSad)
+        {
+            sadButton->setText("Smiling button :)");
+            isSad = false;
+        }
+        else
+        {
+            sadButton->setText("Happy button :D");
+        }
+
+        lineEdit->setText("This button doing nothing, but it glad you clicked on it");
+        });
+
+    window.show();
+    return app.exec();
+}
